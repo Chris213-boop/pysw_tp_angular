87983897b13a59d0bd97a4f3a26459e0bf4980b3
unknown <faculata@gmail.com>
2026-05-11 21:17:45 -0300

    tabla de inscriptos-Facundo Farfan

---
+++ src/app/components/tabla/tabla.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tabla',
+  imports: [CommonModule],
+  templateUrl: './tabla.html',
+  styleUrl: './tabla.css',
+})
+export class Tabla {
+
+}
